ced07cf816d5ea1ab190c88e833148034cd675ac
harshini <harshinipamarthi@gmail.com>
2024-12-27 19:51:19 +0530

    2712

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -2,18 +2,23 @@
 //import the Calculator class
 import com.i27academy.builds.Calculator
 
-//creating an instance of Calculator 
-//pass the current pipeline context using 'this'
- 
-Calculator calculator = new Calculator(this)
-pipeline {
+def call(Map pipelineParams) {
+    //creating an instance of Calculator 
+    //pass the current pipeline context using 'this'
+    Calculator calculator = new Calculator(this)
+
+    pipeline {
     agent any 
+    environment {
+        APP_NAME: ${pipelineParams.appName}
+    }
     stages {
         stage ('Addition Stage') {
             steps {
                 script {
                     echo "printing sum of 2 numbers"
                     println calculator.add(10,20)
+                    echo "My Microservice is: ${APP_NAME}"
                 }
             }
         }
@@ -24,4 +29,7 @@ pipeline {
 
         } 
     }
+    }
 }
+
+
